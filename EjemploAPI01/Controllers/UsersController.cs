6f1ea83cbcf572147342f4b74e89009269c76cca
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-05-14 19:01:02 -0600

    Agregar archivos de proyecto.

---
+++ EjemploAPI01/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using EjemploAPI01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EjemploAPI01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> Users = new List<User>
+        {
+            new User { Id = 1, Name = "Juan", Email = "juan@ejemplo.com" },
+            new User { Id = 2, Name = "Pedro", Email = "pedro@ejemplo.com" },
+            new User { Id = 3, Name = "Rosa", Email = "rosa@ejemplo.com" }
+        };
+
+        // Get: api/users
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return Users;
+        }
+    }
+}
